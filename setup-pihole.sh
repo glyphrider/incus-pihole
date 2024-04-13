527869987406ece748a3471fc92af195f3fb60ce
Brian H. Ward <glyphrider@gmail.com>
2024-04-13 10:20:54 -0400

    working ssl

--- setup-pihole.sh
+++ setup-pihole.sh
@@ -16,3 +16,13 @@ incus file push ./basic-install.sh $HOST/tmp/basic-install.sh -pv --mode 755
 incus exec $HOST -- bash -c /tmp/basic-install.sh
 
 incus exec $HOST -- pihole -a -p
+
+if [ -f ./pihole.key -a -f ./pihole.crt ]; then
+incus file push ./99-ssl.conf $HOST/etc/lighttpd/conf-available/ -pv --mode 644
+incus file push ./pihole.key $HOST/etc/lighttpd/ -pv --mode 600
+incus file push ./pihole.crt $HOST/etc/lighttpd/ -pv --mode 644
+incus file create --type=symlink $HOST/etc/lighttpd/conf-enabled/99-ssl.conf ../conf-available/99-ssl.conf
+incus exec $HOST -- apt-get install lighttpd-mod-openssl
+
+incus exec $HOST -- systemctl restart lighttpd.service
+fi
